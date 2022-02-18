80a773a6f4ad850f298e51220252879134494061
zpoken <vadimoleynik039@gmail.com>
2022-02-18 14:27:08 +0200

    change font

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,9 +1,8 @@
 import { extendTheme } from '@chakra-ui/react';
-import { gold } from '../constants';
 
 export const theme = extendTheme({
   fonts: {
-    heading: 'Arial Rounded MT Bold',
-    body: 'Arial Rounded MT Bold',
+    heading: 'Source Sans Pro',
+    body: 'Source Sans Pro',
   },
 });
